4a8da06599807543dbfbc1e125096d3e403e8671
TamizhanbanT <143093806+TamizhanbanT@users.noreply.github.com>
2023-12-20 12:52:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,89 @@
+
+/* import './App.css';
+import'./style.css';
+
+import JSX from './JSXC';
+import ClassComState from './classcomstate';
+import ClassCom from './classCom';
+import ClassComStateValue from './classchangevalue';
+import Multipleclick from './multipleclickclass';
+/* import Statefun from './statefu'; */
+/* import Parent from './parent'; */
+/* import Increament from './increament';
+import Onmouse from './onmouseincreament';
+import Naveenclass from './naveenclass';
+import FunCom from './naveenfun'; */
+/* import Uncontroll from './uncontroll'; */
+/* import Api from './api'; */
+/* import UseReducer from './reducer'; */
+/* import Inline from './newinline'; */
+
+
+/* import Jeeva from './Funcom'; */
+
+/* import Gg from './button'; */
+
+
+
+import React from 'react';
+/* import PriceCard from './pricechat'; */
+
+/* import Pricecard from "./card"; */
+
+import Anb from './anb';
+
+
+
+
+
+ function App() {
+  debugger
+  return (
+    <div>
+      {/* <Inline></Inline> */}
+     {/*  <UseReducer/> */}
+    {/* <Api/>
+   <ClassComState/>
+   <ClassCom/>
+   <JSX/>
+   <ClassComStateValue/>
+   <Multipleclick/>  */}
+   {/* <button onClick={()=>this.propsparentmethod()}>parent</button> */}
+  {/*  <hr></hr> */}
+    {/* <Increament/>
+   <Onmouse/>
+   <Naveenclass/>
+   <FunCom/> 
+   <ClassCom/> */}
+   {/* <Uncontroll/> */}
+
+
+
+
+    {/* <Jeeva></Jeeva>
+    <Gg/> */}
+
+
+    
+    {/* <div className="container"> */}
+      {/* <div className="row"> */}
+        {/* <div className="col-md-4">
+          <PriceCard />
+        </div> */}
+        {/* Add more PriceCard components for different plans */}
+     
+   
+       {/*  <Pricecard/> */}
+
+
+
+       <Anb></Anb>
+
+       
+
+   </div>
+  );
+  
+}
+
+export default App;
